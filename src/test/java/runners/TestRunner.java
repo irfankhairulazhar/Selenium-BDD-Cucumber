7f8e5d1c34b36c5dccefbecd2a784c710ba9c374
irfankhairulazhar <ikirfan60@gmail.com>
2024-03-31 16:02:42 +0700

    update report name

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -2,7 +2,7 @@ package runners;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        plugin = {"json:target/results/testLogisly/cucumber-report.json", "html:target/results/testLogisly"},
+        plugin = {"json:target/results/swagLabs/cucumber-report.json", "html:target/results/swagLabs"},
         features = "src/test/resources/features",
         glue = "steps",
         tags = {"@all"}
